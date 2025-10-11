439dc77c1d178a9a92106af71856f9f09ec4d692
SanychSan <alexandr.lupa@gmail.com>
2025-10-11 09:43:58 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ion-cp-a-cp_dashboard',
+  webDir: 'www'
+};
+
+export default config;
