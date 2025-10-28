3c8a02b2745c0153af55a8e35c1614722f7df1ca
Aleksandr Lupa <alexandr.lupa@gmail.com>
2025-10-28 23:06:47 +0100

    spot ui

--- src/app/components/spot-table/spot-table-mobile/spot-table-mobile.component.ts
+++ src/app/components/spot-table/spot-table-mobile/spot-table-mobile.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
-// import { IonText } from '@ionic/angular/standalone';
+import { IonText } from '@ionic/angular/standalone';
 
 import { CoinInterface } from 'src/app/classes/coin';
 import { SpotTable } from '../spot-table';
@@ -13,7 +13,7 @@ import { SpotTable } from '../spot-table';
   styleUrls: ['./spot-table-mobile.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, MatTableModule, MatSortModule]
+  imports: [CommonModule, MatTableModule, MatSortModule, IonText]
 })
 export class SpotTableMobileComponent extends SpotTable {
   override displayedColumns = ['currency', 'price', 'profit'];
